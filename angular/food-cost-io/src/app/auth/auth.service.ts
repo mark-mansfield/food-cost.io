fd86151dcea899aa17127edcb16c9ffb91ca87cd
mark-mansfield <markgmansfield@gmail.com>
2018-10-09 15:22:18 +1100

    removed console messages that were used for debugging an issue

--- angular/food-cost-io/src/app/auth/auth.service.ts
+++ angular/food-cost-io/src/app/auth/auth.service.ts
@@ -24,7 +24,6 @@ export class AuthService {
   }
 
   getIsAuth() {
-    console.log('GetIsAuth Says... ' + this.isAuthenticated);
     return this.isAuthenticated;
   }
 
--- angular/food-cost-io/src/app/dishes/dishes-list/dishes-list.component.ts
+++ angular/food-cost-io/src/app/dishes/dishes-list/dishes-list.component.ts
@@ -13,15 +13,18 @@ import { Dish } from '../dish.model';
 export class DishesListComponent  implements OnInit, OnDestroy  {
 
   dishes: Dish[] = [];
+  isLoading = false;
   private dishesSub: Subscription;
 
   constructor(public dishesService: DishService) {}
 
   ngOnInit() {
+    this.isLoading = true;
     this.dishesService.getDishes();
     this.dishesSub = this.dishesService.getDishUpdateListener()
       .subscribe((dishes: Dish[]) => {
         this.dishes = dishes;
+        this.isLoading = false;
       });
   }
 
