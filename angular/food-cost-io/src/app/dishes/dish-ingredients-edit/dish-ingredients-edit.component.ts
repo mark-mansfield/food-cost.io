1a4944ac79de249100b2f8d1dee2f3ad7adc135a
mark-mansfield <markgmansfield@gmail.com>
2018-10-11 11:53:43 +1100

    initial commit for  dish ingredients edit component

---
+++ angular/food-cost-io/src/app/dishes/dish-ingredients-edit/dish-ingredients-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-ingredients-edit',
+  templateUrl: './dish-ingredients-edit.component.html',
+  styleUrls: ['./dish-ingredients-edit.component.css']
+})
+export class DishIngredientsEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- angular/food-cost-io/src/app/dishes/dishes-routing.module.ts
+++ angular/food-cost-io/src/app/dishes/dishes-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DishCreateComponent } from './dish-create/dish-create.component';
 import { DishesListComponent } from './dishes-list/dishes-list.component';
 import { DishDetailsComponent } from './dish-details/dish-details.component';
+import { DishIngredientsEditComponent } from './dish-ingredients-edit/dish-ingredients-edit.component';
 import { AuthGuard } from '../auth/auth.guard';
 import { EditFieldComponent } from './edit-field/edit-field.component';
 
@@ -10,8 +11,8 @@ const dishesRoutes: Routes = [
   { path: 'dishes',  component: DishesListComponent, canActivate: [AuthGuard]},
   { path: 'dish/create', component: DishCreateComponent ,  canActivate: [AuthGuard]},
   { path: 'dish/:_id', component: DishDetailsComponent ,  canActivate: [AuthGuard]},
-  { path: 'dish/edit-field/:_id/:field_name/:field_type', component: EditFieldComponent ,  canActivate: [AuthGuard]}
-  { path: 'dish/:_id/ingredients', component: EditFieldComponent ,  canActivate: [AuthGuard]}
+  { path: 'dish/edit-field/:_id/:field_name/:field_type', component: EditFieldComponent ,  canActivate: [AuthGuard]},
+  { path: 'dish/:_id/ingredients', component: DishIngredientsEditComponent ,  canActivate: [AuthGuard]}
 ];
 
 @NgModule({
