6aedc4369a87473010745741481487ac762c5e48
mark-mansfield <markgmansfield@gmail.com>
2018-10-28 12:45:58 +1100

    added index lik list front end

--- angular/food-cost-io/src/app/ingredients/ingredients-list/ingredients-list.component.ts
+++ angular/food-cost-io/src/app/ingredients/ingredients-list/ingredients-list.component.ts
@@ -5,34 +5,34 @@ import { IngredientsService } from '../ingredients.service';
 import { Ingredient } from '../ingredient.model';
 import { CompileShallowModuleMetadata } from '@angular/compiler';
 
-
 @Component({
   selector: 'app-ingredients-list',
   templateUrl: './ingredients-list.component.html',
   styleUrls: ['./ingredients-list.component.css']
 })
 export class IngredientsListComponent implements OnInit, OnDestroy {
-
   public isLoading = false;
   public showRefresh = false;
   public ingredients: Ingredient[] = [];
   public linksList = [];
   public searchTerm: string;
   private ingredientsSub: Subscription;
-
+  public ingredientData;
   constructor(
     private service: IngredientsService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     // preloading ingredients data to local storage is  done in auth.service.
-    const ingredientData = this.service.loadLocalIngredientsData();
-    console.log(ingredientData);
-    if (ingredientData) {
-      this.ingredients = ingredientData.ingredients;
+    this.ingredientData = this.service.loadLocalIngredientsData();
+
+    console.log(this.ingredientData);
+    if (this.ingredientData) {
+      this.ingredients = this.ingredientData.ingredients;
       this.isLoading = false;
+      this.buildLinksList();
       this.ingredientsSub = this.service
         .geIngredientsUpdateListener()
         .subscribe();
@@ -44,18 +44,37 @@ export class IngredientsListComponent implements OnInit, OnDestroy {
         .geIngredientsUpdateListener()
         .subscribe((data: Ingredient[]) => {
           this.ingredients = data;
-          // this.buildLinksList();
+          this.buildLinksList();
           this.isLoading = false;
         });
     }
-
   }
 
-  search(searchvalue) { }
+  // links list
+  buildLinksList() {
+    if (this.linksList.length === 0) {
+      const tmpArray = [];
+      // const localIngredients: any = this.service.loadLocalIngredientsData();
+      console.log(this.ingredientData.ingredients);
 
-  refresh() { }
+      this.ingredients.sort().forEach(item => {
+        tmpArray.push(item.name.substring(0, 1).toLocaleLowerCase());
+      });
+      this.linksList = Array.from(new Set(tmpArray));
+      this.linksList.sort();
+      console.log(this.linksList);
+    }
+  }
 
-  searchByFirstletter(link) { }
+  search(searchvalue) {
+    console.log(searchvalue);
+  }
+
+  refresh() {}
+
+  searchByFirstletter(link) {
+    console.log(link);
+  }
 
   onDelete(index) {
     console.log(index);
--- git commands.md
+++ git commands.md
@@ -1,16 +1,17 @@
 # Pull repo from git
 
 1. cd into local directory
-2. initialize new repo ``$ git init``
-3. clone repository `` git clone <url>``
+2. initialize new repo `$ git init`
+3. clone repository `git clone <url>`
 4.
 
 # git-workflow
+
 work-flow learning for using github
 
 Before creating a new branch, pull the changes from upstream. Your master needs to be up to date.
 
-Create the branch on your local machine and switch in this branch :
+Create git
 
 ```
 $ git checkout -b [name_of_your_new_branch]
@@ -21,6 +22,7 @@ Change working branch :
 ```
 $ git checkout [name_of_your_new_branch]
 ```
+
 Push the branch on github :
 
 ```
@@ -33,7 +35,6 @@ You can see all branches created by using :
 git branch
 ```
 
-
 # Merging Branches
 
 You can run your tests, make sure the hotfix is what you want, and finally merge the hotfix branch back into your master branch to deploy to production. You do this with the git merge command:
@@ -43,8 +44,14 @@ $ git checkout [default branch]
 $ git merge hotfix
 ```
 
-# delete branch after use
+# delete local branch after use
 
 ```
 $ git branch -d [hotfix]
+```
+
+# delete remote branch after use
+
+```
+$git git push origin --delete [branch_name]
 ```
